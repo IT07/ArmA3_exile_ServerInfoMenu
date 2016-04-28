9547ba71153a6b87a02c3ac78e6864b50c500347
IT07 <it07@scarcode.nl>
2016-04-28 14:13:36 +0200

    openKey and menuItems changes

--- Exile.MapName/scarCODE/ServerInfoMenu/hpp/CfgServerInfoMenu.hpp
+++ Exile.MapName/scarCODE/ServerInfoMenu/hpp/CfgServerInfoMenu.hpp
@@ -3,43 +3,50 @@ class CfgServerInfoMenu
    antiHACK = "infiSTAR + BattlEye";
    hostedBy = "GamerClanName";
    ipPort = "123.456.789:2302";
+   openKey = "User7"; // https://community.bistudio.com/wiki/inputAction/actions
    restart = 3; // Amount of hours before server automatically restarts
    serverName = "Name of this server";
    class menuItems
    {
 
-      class General
+      class first
       {
+         menuName = "General";
          title = "GENERAL INFORMATION";
          content[] = {
             "This is some placeholder text"
          };
       };
-      class Rules
+      class second
       {
+         menuName = "Rules";
          title = "SERVER RULES";
          content[] = {
             "<t size='1.75'>Rule #1</t><br />Players/admins are NOT permitted to show racism, profanity and disrespect towards any other player/admin in this server.<br />",
             "<t size='1.75'>Rule #2</t><br />No voice transmission over side chat. Adjust your control settings to prevent doing it by accident.<br />"
          };
       };
-      class Crafting
+      class third
       {
+         menuName = "Crafting";
          title = "CRAFTING GUIDE";
          content[] = {""};
       };
-      class Admins
+      class fourth
       {
+         menuName = "Admins";
          title = "ADMINISTRATORS";
          content[] = {""};
       };
-      class Scripts
+      class fifth
       {
+         menuName = "Scripts";
          title = "INSTALLED SCRIPTS";
          content[] = {""};
       };
-      class Communication
+      class sixth
       {
+         menuName = "Communication";
          title = "VOICE SERVER AND WEBSITE";
          content[] = {
             "<t size='1.25'>TeamSpeak:</t> ts.website.com",
@@ -47,19 +54,15 @@ class CfgServerInfoMenu
             "<t size='1.25'>Website:</t> https://website.com"
          };
       };
-      class Credits
+      class seventh
       {
+         menuName = "Credits";
          title = "CREDITS WHERE CREDITS DUE";
          content[] = {
             "<t size='1.25'>IT07:</t> coder/creator of this menu",
             "<br />",
             "<t size='1.25'>Name:</t> blablabla"
          };
-      };
-      class Other
-      {
-         title = "OTHER INFORMATION";
-         content[] = {""};
       };
    };
 };
